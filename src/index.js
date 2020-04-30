14a022eca8c3dfdb25a207aa0a5f710c0c6aa511
Timo Bechtel <timo@bechtel.solutions>
2020-04-30 18:37:49 +0200

    refactor: split main file into smaller files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'reveal.js/css/reveal.css';
 import 'reveal.js/css/theme/black.css';
 import 'highlight.js/styles/dracula.css';
 import Reveal from 'reveal.js';
-import hljs from 'highlight.js';
 
 Reveal.initialize({
   controls: true,
@@ -10,8 +9,4 @@ Reveal.initialize({
   history: true,
   center: true,
   transition: 'none',
-});
-
-document.querySelectorAll('pre code').forEach((block) => {
-  hljs.highlightBlock(block);
 });
